f4fcaad174e365022a29441712c44ede5ff2c15b
Facundo <facundo.kisser@gmail.com>
2023-12-19 20:15:47 -0300

    a verr

--- database/config.ts
+++ database/config.ts
@@ -3,12 +3,14 @@ import mongoose from "mongoose";
 export const dbConnection = async (): Promise<void> => {
 	try {
 		const dbURL = process.env.DB_URL;
+		console.log(dbURL);
 		if (!dbURL) {
 			throw new Error(
 				"La URL de la DB no está correctamente definida en las variables de entorno"
 			);
 		}
 		await mongoose.connect(dbURL);
+		console.log("BDD conectada");
 	} catch (error) {
 		console.log(error);
 		throw new Error("Error al momento de iniciar la DB");
